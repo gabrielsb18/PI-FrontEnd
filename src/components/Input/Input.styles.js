e28399d2dc75fb5c5cb426db794c985b7e1bf41e
Rainier Viana <rainierviana@MacBook-Pro-de-Rainier.local>
2024-09-11 22:20:48 -0300

    Criar componente Input

---
+++ src/components/Input/Input.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const InputWrap = styled.div`
+    width: 100%;
+    display: flex;
+    align-item: center;
+    justify-content: center;
+    border-radius: 8px;
+    background: #E4E4E4;
+
+    >input{
+        height: 54px;
+        width: 100%;
+        background:none;
+        border:none;
+        padding: 20px;
+    }
+`;
