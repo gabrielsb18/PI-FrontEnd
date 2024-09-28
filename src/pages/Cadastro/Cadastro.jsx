83bc970e8bec95190b7953e8633ef5232155577e
Rainier Viana <rainierviana@MacBook-Pro-de-Rainier.local>
2024-09-27 21:30:31 -0300

    Criação da página Cadastro (pendente de estilização)

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,18 +1,15 @@
-import {ContainerBody, Title, FormContainer } from '../Login/Login.style'
+import {ContainerBody, Title, FormContainer } from '../Cadastro/Cadastro.style'
 import { PanelLogo } from '../../components/PaneLogo/panelLogo'
 import Input from "../../components/Input/Input"
 import Button from '../../components/Button/Button'
-import { useNavigate } from 'react-router-dom';
 
-export default function Login (){
-    const navigate = useNavigate();
-
+export default function Cadastro (){
     return (
         <ContainerBody>
             <FormContainer>
                 <Title>
-                    <h1>Login</h1>
-                    <p>Faça login para continuar</p>
+                    <h1>Bem vindo ao Notes</h1>
+                    <p>Cadastre- se para aproveitar ao máximo  o Notes</p>
                 </Title>
 
                 <form>
@@ -21,8 +18,7 @@ export default function Login (){
                         <Input nome="Senha" />
                     </div>
                     <div>
-                        <Button estilo="flat" name="Entrar"/>
-                        <Button estilo="outlined" name="Cadastre-se" onClick={() => navigate('/cadastro')}/>
+                        <Button estilo="flat" name="Cadastre-se"/>
                     </div>
                 </form>
             </FormContainer>
