e65232c774d27617f65628a457af1d89f7fc6175
gabrielsb18 <gbmodelator@gmail.com>
2024-10-20 13:37:21 -0300

    Adicionando a prop icon ao input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import { InputWrap } from "./Input.styles";
 
-export default function Input({ nome, ...rest }) {
+export default function Input({ nome, icon:Icon, ...rest }) {
     return (
         <>
             <label>{nome}</label>
             <InputWrap>
+                {Icon && <Icon size={20} />}
                 <input {...rest}/>
             </InputWrap>
         </>
