b795be75b022d660674b61804ab4bd088afa8dfd
gabrielsb18 <gbmodelator@gmail.com>
2024-10-20 13:38:40 -0300

    Definindo icone dos inputs na tela de cadastro e o link de redirecionamento caso tenha uma conta

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -2,20 +2,32 @@ import {ContainerBody, Title, FormContainer } from '../Cadastro/Cadastro.style'
 import { PanelLogo } from '../../components/PaneLogo/panelLogo'
 import Input from "../../components/Input/Input"
 import Button from '../../components/Button/Button'
+import { MdOutlineEmail,MdOutlineLock } from "react-icons/md";
+import { Link } from 'react-router-dom';
 
 export default function Cadastro (){
     return (
         <ContainerBody>
             <FormContainer>
                 <Title>
-                    <h1>Bem vindo ao Notes</h1>
-                    <p>Cadastre- se para aproveitar ao máximo  o Notes</p>
+                    <h1>Bem vindo ao <br/><strong>Notes</strong></h1>
+                    <p>Cadastre- se para aproveitar ao máximo o Notes</p>
                 </Title>
 
                 <form>
                     <div>
-                        <Input nome="Email" />
-                        <Input nome="Senha" />
+                        <Input
+                            icon={MdOutlineEmail} 
+                            nome="Email"
+                            placeholder="Digite seu email"
+                        />
+                        <Input
+                            icon={MdOutlineLock}
+                            nome="Senha"
+                            placeholder="Digite sua senha"
+                        />
+
+                        <Link to="/">Já tem uma conta? <strong>Entrar</strong></Link>
                     </div>
                     <div>
                         <Button estilo="flat" name="Cadastre-se"/>
