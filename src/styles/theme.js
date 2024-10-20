072b6aec8fa94e400f57444735c21e439cc00c19
gabrielsb18 <gbmodelator@gmail.com>
2024-10-20 12:57:15 -0300

    adicionando restante das cores da paleta

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,17 @@
-//DEFININDO PALETA DE CORES DA NOSSA APLICAÇÃO
-
 export default {
     COLORS: {
         BACKGROUND_COLOR: "#E4E4E4",
         TEXT_COLOR: "#000",
-        YELLOW_DEFAULT: "#FFD735"
+        YELLOW_DEFAULT: "#FFD735",
+        YELLOW_100: "#FFFBEB",
+        YELLOW_200: "#FFF9E1",
+        YELLOW_300: "#FFF3C0",
+        YELLOW_400: "#FFD735",
+        YELLOW_500: "#E6C230",
+        YELLOW_600: "#CCAC2A",
+        YELLOW_700: "#BFA128",
+        YELLOW_800: "#998120",
+        YELLOW_900: "#736118",
+        YELLOW_1000: "#594B13"
     }
 }
