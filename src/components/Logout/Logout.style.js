684ecc3d3bcd73df589491f4063589e4331fe10b
Rainier Github <RBDSVIANA@MacBook-Pro-de-Rainier.local>
2024-11-06 21:26:32 -0300

    Atualizando botão logout

--- src/components/Logout/Logout.style.js
+++ src/components/Logout/Logout.style.js
@@ -5,4 +5,10 @@ export const LogoutButton = styled.button`
     height:34px;
     background-color: transparent;
     border: none;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin-right: 4rem;
+    margin-bottom: 3rem;
 `;
+
