e4414f65a1aa02a259f630e1de79a6db3087e601
gabrielsb18 <gbmodelator@gmail.com>
2024-11-14 16:37:05 -0300

    Adicionando Link de redirecionamento para a home, ao clickar na logo do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { ButtonProfile } from "../ButtonProfile/ButtonProfile";
 import { Header } from "./Header.style";
 import { SearchBar } from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
 
 export default function HeaderNav({showSearchBar = true}) {
 	return (
 		<Header>
-			<img src="/IconLogo.svg" alt="Logo Notes" />
+            <Link to = "/home">
+                <img src="/IconLogo.svg" alt="Logo Notes"/>
+            </Link>
 			{showSearchBar && <SearchBar placeholder="Pesquisar notas" />}
 			<ButtonProfile />
 		</Header>
