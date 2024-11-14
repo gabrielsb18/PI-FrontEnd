cb9a293f313bbb560eb0cd7f4ff4a11b048597be
gabrielsb18 <gbmodelator@gmail.com>
2024-11-14 17:26:24 -0300

    Provendo contexto para as rotas de acesso

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import {Routes} from "./routes/index.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import { Routes } from "./routes/index.jsx";
 
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<AuthProvider>
+				<Routes />
+			</AuthProvider>
+		</ThemeProvider>
+	</StrictMode>,
 );
