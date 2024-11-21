1e5ed826b799d9e4940a3e9a69d0cfe4aadcd0f7
gabrielsb18 <gbmodelator@gmail.com>
2024-11-21 02:54:47 -0300

    ajustando estilos do botão AddTask para melhor responsividade e usabilidade

--- src/components/AddTask/AddTask.style.js
+++ src/components/AddTask/AddTask.style.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
+import { device } from "../../constants/index";
 
 export const Button = styled.button`
-    width:150px;
-    height:52px;
+    width: 100%;
+    height: 52px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
 
-    border: 1px solid #979797;
+    border: 2px solid #979797;
     border-radius: 10px;
     background-color: transparent ;
     
@@ -23,4 +29,9 @@ export const Button = styled.button`
         transition: ease-in-out 0.2s;
         opacity: 0.4;
     }
+
+    @media ${device.xs}{
+        max-width: 150px;
+        font-size: 1em;
+    } 
 `;
