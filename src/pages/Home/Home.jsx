99771d9014815f452cbff7a1df7a978b78898fbd
gabrielsb18 <gbmodelator@gmail.com>
2024-11-21 18:54:11 -0300

    Closes #110 Fazendo integração da tela home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,10 @@ import Select from "../../components/Select/Select";
 import Logout from "../../components/Logout/Logout";
 import { CardTask } from "../../components/CardTasks/CardTasks.jsx";
 import { ToastPopUp } from "../../components/Toast/Toast.jsx";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext.jsx";
+import { api } from "../../services/api.js";
+import { CardTaskModal } from "../../components/Modal/CardTaskModal.jsx";
 
 import {
 	ContainerBody,
@@ -17,16 +21,56 @@ import {
     ActionsButtons,
 	ContainerTitle,
 	ContainerCardsTasks,
+    ContainerTasks,
+    BackgroundModal,
 } from "./Home.style";
 
+
 export default function Home() {
+    const { userId, nome } = useAuth();
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [currentNote, setCurrentNote] = useState(null);
+    const [notes, setNotes] = useState([]);
+
+    useEffect(()=> {
+        async function getNotes(){
+            const response = await api.get(`/notes/${userId}`)
+            setNotes(response.data)
+        }
+
+        getNotes();
+    }, [])
+
+    const updateNote = (updateNote) => {
+        setNotes((prevNotes) => 
+            prevNotes.map((note) =>
+                note._id === updateNote._id ? updateNote : note
+            ) 
+        )
+    };
+
+    const deleteNote = (noteId) => {
+        setNotes((prevNotes) => prevNotes.filter((note) => note._id !== noteId));
+    };
+
+    const handleCardClick = (note) => {
+        setCurrentNote(note);
+        setModalOpen(true);
+    }
+
+    const handleCloseModal = () => {
+        setCurrentNote(null);
+        setModalOpen(false);
+    }
+
 	return (
 		<ContainerBody>
 			<HeaderNav />
 			<Main>
 				<HeaderTitle>
 					<h1>
-						Olá <strong>Fulano</strong> 👋
+						Olá <strong>{nome}</strong> 👋
 					</h1>
 					<p>Adicione uma nova tarefa</p>
 				</HeaderTitle>
@@ -56,11 +100,35 @@ export default function Home() {
                         </ActionsButtons>
 					</ContainerTitle>
 				</HeaderTasks>
-				<ContainerCardsTasks>
-					<CardTask title="teste" description="teste" />
-					<CardTask title="teste" description="teste" />
-					<CardTask title="teste" description="teste" />
-				</ContainerCardsTasks>
+
+                <ContainerTasks>
+                    <ContainerCardsTasks>
+                        {notes.map((note => (
+                            <CardTask
+                                title={note.titulo}
+                                description={note.descricao}
+                                status={note.status}
+                                onClick={()=>handleCardClick(note)}
+                                key={note._id}
+                            />
+                        )))}
+                    </ContainerCardsTasks>
+                        {currentNote && modalOpen && (
+                            <>
+                                <BackgroundModal onClick={handleCloseModal}/>
+                                <CardTaskModal
+                                    onUpdate={updateNote}
+                                    onDelete={deleteNote}
+                                    noteId={currentNote._id}
+                                    isOpen={modalOpen}
+                                    onClose={handleCloseModal}
+                                    key={currentNote._id}
+                                    title={currentNote.titulo}
+                                    content={currentNote.descricao}
+                                />
+                            </>
+                        )}
+                </ContainerTasks>
 			</Main>
 			<Logout />
 			<ToastPopUp />
