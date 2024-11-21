a8cab3614c671a6bf68bf900801d362b83450398
gabrielsb18 <gbmodelator@gmail.com>
2024-11-21 03:48:57 -0300

    adicionando redirecionamento para a página de login após clicar no botão logout

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,11 +1,18 @@
 import { LogoutButton } from "./Logout.style"
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function AddTask(){
     const {signOut} = useAuth();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        signOut();
+        navigate("/login");
+    }
 
     return(
-        <LogoutButton onClick={signOut} >
+        <LogoutButton onClick={handleClick} >
             <img src="../../public/logout.svg" />
         </LogoutButton>
     )
