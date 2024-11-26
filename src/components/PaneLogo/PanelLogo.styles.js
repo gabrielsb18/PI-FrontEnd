05b03d5600d24aaec7ee9a7cf600dec8fd9ae478
Rainier Github <RBDSVIANA@MacBook-Pro-de-Rainier.local>
2024-11-25 22:49:20 -0300

    closes #116 Ajustar responsividade da tela de login

--- src/components/PaneLogo/PanelLogo.styles.js
+++ src/components/PaneLogo/PanelLogo.styles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import {device} from "../../constants/index"
 
 export const Container = styled.div`
     display: flex;
+    flex:2;
     width:50%;
     height: 100vh;
     flex-direction: column;
@@ -9,4 +11,12 @@ export const Container = styled.div`
     align-items: center;
     overflow: hidden;
     background-color: #FFD735;
+
+    @media ${device.xs}{
+        display: none;
+    }
+
+    @media ${device.sm}{
+        display: none;
+    }
 `;
