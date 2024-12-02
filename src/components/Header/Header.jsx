ff47c7ebf0df61127b96283df3489ae2a0b83b29
gabrielsb18 <gbmodelator@gmail.com>
2024-12-02 11:17:30 -0300

    Adicionando suporte para manipulação de busca no componente HeaderNav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,25 @@ import { Header } from "./Header.style";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { Link } from "react-router-dom";
 
-export default function HeaderNav({showSearchBar = true}) {
+export default function HeaderNav({ showSearchBar = true, onSearch }) {
+	
+    const handleInputChange = (e) => {
+		onSearch(e.target.value);
+	};
+
 	return (
 		<Header>
-            <Link to = "/home">
-                <img src="/IconLogo.svg" alt="Logo Notes"/>
-            </Link>
-			{showSearchBar && <SearchBar placeholder="Pesquisar notas" />}
+			<Link to="/home">
+				<img src="/IconLogo.svg" alt="Logo Notes" />
+			</Link>
 
-            <ButtonProfile />
+			{showSearchBar && (
+				<SearchBar
+					onChange={handleInputChange}
+					placeholder="Pesquisar notas"
+				/>
+			)}
+			<ButtonProfile />
 		</Header>
 	);
 }
