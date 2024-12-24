0bbce450f1ae4bf4ebf46235783c411e52d6d4a2
gabrielsb18 <gbmodelator@gmail.com>
2024-12-24 19:22:16 -0300

    fix: Ajusta largura máxima do cabeçalho para 100%

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -4,6 +4,7 @@ export const Header = styled.header`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    max-width: 100%;
     
     width: 100vw;
     height: 4rem;
