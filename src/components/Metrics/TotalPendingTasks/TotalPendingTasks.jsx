85afbade6b4edd38e56b6ed1967fefad8d2f967e
gabrielsb18 <gbmodelator@gmail.com>
2025-01-12 00:12:19 -0300

    refactor: remover chamadas para api nas funções TotalCompletedTasks e TotalPendingTasks

--- src/components/Metrics/TotalPendingTasks/TotalPendingTasks.jsx
+++ src/components/Metrics/TotalPendingTasks/TotalPendingTasks.jsx
@@ -1,30 +1,17 @@
-import { useEffect, useState } from "react";
 import { ContentGraphicTwo } from "./TotalPendingTasks.styles.js";
-import { getMetrics } from "../../../services/metricsService.js";
-import { toast } from "sonner";
+import { useMetrics } from "../../../contexts/MetricsContext";
 
-export function TotalPendingTasks() {
-    const [totalTasks, setTotaltasks] = useState();
-
-    useEffect(() => {
-        const getTotalPendingTasks = async () => {
-            try {
-                const response = await getMetrics();
-                setTotaltasks(response.pendente)
-            } catch (error) {
-                toast.error(error.message)
-            }
-        }
-
-        getTotalPendingTasks();
-    }, [totalTasks])
+function TotalPendingTasks() {
+	const { totalTasksPending } = useMetrics();
 
 	return (
 		<ContentGraphicTwo>
 			<p>
 				Tarefas <br /> <strong>Pendentes</strong>
 			</p>
-			<h1>{totalTasks}</h1>
+			<h1>{totalTasksPending}</h1>
 		</ContentGraphicTwo>
 	);
 }
+
+export { TotalPendingTasks };
